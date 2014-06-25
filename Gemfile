ca71c9d8c65e75ce32a267a6aeb44a4960311e60
Krishna Kulkarni <krishna.kulkarni19@gmail.com>
2014-06-25 00:26:43 -0400

    State of app after Tutorial1

--- Gemfile
+++ Gemfile
@@ -44,7 +44,7 @@ end
 # gem 'capistrano', group: :development
 
 # Use debugger
-gem 'debugger', group: [:development, :test]
+#gem 'debugger', group: [:development, :test]
 
 group :development do
   gem 'binding_of_caller'
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -15,4 +15,19 @@ class StaticPagesController < ApplicationController
     redirect_to root_url, notice: "CSV Imported"
   end
   
+  def enter_record
+    render :enter_record
+  end
+  
+  def create_record
+    @first_name = params["idp"]["first_name"]
+    
+    TestIdp.create!(idp_params)
+    render :enter_record
+  end
+  
+  def idp_params
+    params.require(:idp).permit(:first_name, :last_name)
+  end
+  
 end
